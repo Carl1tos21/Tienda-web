9621d286490307cb592bd7fbfe4a0577a0cb19df
Carlos Daniel <turbofloreschilo@gmail.com>
2025-12-23 13:06:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let products = [];
+
+function renderProducts() {
+    const list = document.getElementById("product-list");
+    list.innerHTML = "";
+
+    products.forEach((product, index) => {
+        list.innerHTML += `
+            <div class="product">
+                <span>${product.name} - Bs. ${product.price}</span>
+                <button onclick="editPrice(${index})">Editar</button>
+            </div>
+        `;
+    });
+}
+
+function addProduct() {
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+
+    if (name === "" || price === "") {
+        alert("Completa todos los campos");
+        return;
+    }
+
+    products.push({ name, price });
+    renderProducts();
+
+    document.getElementById("name").value = "";
+    document.getElementById("price").value = "";
+}
+
+function editPrice(index) {
+    const newPrice = prompt("Nuevo precio:");
+    if (newPrice !== null) {
+        products[index].price = newPrice;
+        renderProducts();
+    }
+}
